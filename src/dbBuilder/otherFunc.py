24964e5f3dd52346ca079679ea5ca955097b9b0b
Nazaryan ART <84046495+SL1dee36@users.noreply.github.com>
2024-11-03 15:37:12 +0300

    v2.1.0

---
+++ src/dbBuilder/otherFunc.py
@@ -0,0 +1,20 @@
+from webbrowser import open_new_tab as ont
+
+def githubLink():
+    ont("https://github.com/atxxxm/UFO-DB-APP")
+
+def gitlink():
+    ont("https://github.com/SL1DEE36/UFO-DB-APP")
+    
+def AuthorLink():
+    ont("https://github.com/atxxxm")
+    ont("https://github.com/Sl1dee36")
+
+def NewsLink():
+    pass
+
+def openPrivacyPolicy():
+    ont("https://github.com/atxxxm/UFO-DB-APP/blob/main/Privacy%20Policy.md")
+
+def openTermsofUse():
+    ont("https://github.com/atxxxm/UFO-DB-APP/blob/main/TermsOfUse.md")
